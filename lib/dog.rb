477ba7f153510460fb1e68dc36900bb525066400
Kevin <kevinarogers1@gmail.com>
2021-02-14 11:44:51 -0500

    commit tree 62052f1275ca1a8d01b4c26bc73bb0ca5e413eca

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,14 +4,10 @@ class Dog
         @breed = breed
     end
 
-    def name= (name)
+    def name (name)
         @name = name
     end
-
-    def name
-        @name
-    end
-
+    
     def breed (breed) 
         @breed = breed
     end
